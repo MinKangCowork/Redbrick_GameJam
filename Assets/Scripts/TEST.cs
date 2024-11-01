34bacb9f49ec98cae12ce2b052640a00ce1772b3
rlatjrqja <0sogogi0@naver.com>
2024-11-01 20:07:34 +0900

    [Add] DOTween 추가
    https://assetstore.unity.com/packages/tools/animation/dotween-hotween-v2-27676

---
+++ Assets/Scripts/TEST.cs
@@ -0,0 +1,22 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static DG.Tweening.DOTween;
+
+public class TEST : MonoBehaviour
+{
+    GameObject obj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
